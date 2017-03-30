b567003db5d3942fc3449dd41f134e2b08c1a956
HuyLafa <anhhuy0706@gmail.com>
2017-03-30 01:39:26 -0400

    Add button to convert between normal text input and math input.

--- public/javascripts/math_input.js
+++ public/javascripts/math_input.js
@@ -3,7 +3,24 @@
 * Source: https://github.com/HuyLafa/MathTutor
 */
 
-$(document).ready(function() {
+$(document).ready( function() {
+  setupMathInput();
+  // switch between normal text and math input
+  $('#typeMath').change( function() {
+	$('#inputArea').html("");
+	if (this.checked) {
+		$textarea = $("<div id='mathquill'></div>").appendTo('#inputArea');
+		setupMathInput();
+	}
+	else {
+		$textarea = $("<textarea class='form-control' placeholder='type a message'></textarea>").appendTo('#inputArea');
+		closeKeyboard();
+	}
+  });
+});
+
+function setupMathInput() {
+  // math input configuration
   var isMobile = window.matchMedia("only screen and (max-width: 760px)");
   var MQElement = document.getElementById("mathquill");
 
@@ -13,66 +30,65 @@ $(document).ready(function() {
 
   $('#keyboard .keypad:not(.default)').hide();
   $('#keyboard .key').click( function(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    if ( $(this).data('action') == 'write') {
-      mathField.write($(this).data('content'));
-    } else if($(this).data('action') == 'cmd') {
-      mathField.cmd($(this).data('content'));
-    } else if($(this).data('action') == 'keystroke') {
-      mathField.keystroke($(this).data('content'));
-    } else if($(this).data('action') == 'switch-keys') {
-      console.log("'#keyboard .keypad:not(.' + $(this).data('content') + ')'");
-      $('#keyboard .keypad:not(.' + $(this).data('content') + ')').hide();
-      $('#keyboard .keypad.' + $(this).data('content')).show();
-    } else if($(this).data('action') == 'keyboard-hide'){
-      closeKeyboard();
-    }
+	if ( $(this).data('action') == 'write') {
+	  mathField.write($(this).data('content'));
+	} else if($(this).data('action') == 'cmd') {
+	  mathField.cmd($(this).data('content'));
+	} else if($(this).data('action') == 'keystroke') {
+	  mathField.keystroke($(this).data('content'));
+	} else if($(this).data('action') == 'switch-keys') {
+	  console.log("'#keyboard .keypad:not(.' + $(this).data('content') + ')'");
+	  $('#keyboard .keypad:not(.' + $(this).data('content') + ')').hide();
+	  $('#keyboard .keypad.' + $(this).data('content')).show();
+	} else if($(this).data('action') == 'keyboard-hide'){
+	  closeKeyboard();
+	}
 
-    if (typeof $(this).data('stepback') !== 'undefined') {
-      for (var i = 0; i < parseInt($(this).data('stepback')); i++) {
-        mathField.keystroke('Left');
-      }
-    }
+	if (typeof $(this).data('stepback') !== 'undefined') {
+	  for (var i = 0; i < parseInt($(this).data('stepback')); i++) {
+		mathField.keystroke('Left');
+	  }
+	}
 
-    if (typeof $(this).data('stepforward') !== 'undefined') {
-      for (var i = 0; i < parseInt($(this).data('stepforward')); i++) {
-        mathField.keystroke('Right');
-      }
-    }
+	if (typeof $(this).data('stepforward') !== 'undefined') {
+	  for (var i = 0; i < parseInt($(this).data('stepforward')); i++) {
+		mathField.keystroke('Right');
+	  }
+	}
 
-    if(isMobile) {
-      mathField.blur();
-    } else {
-      mathField.focus();
-    }
+	if(isMobile) {
+	  mathField.blur();
+	} else {
+	  mathField.focus();
+	}
   });
-
-  closeKeyboard = function() {
-    $('#keyboard-mask').slideUp();
-    $('#keyboard-wrapper').slideUp();
-  }
-
-  openKeyboard = function() {
-    $('#keyboard-wrapper').slideDown();
-    $('#keyboard-mask').slideDown("fast", function() {
-      $(window).scrollTop($('#keyboard-mask').position().top + $('#keyboard-mask').outerHeight());
-    });
-  }
 
   closeKeyboard();
 
   $('#mathquill').click(function(e) {
-    //$('.mq-textarea textarea').attr('readonly', 'readonly'); // Force keyboard to hide on input field.
-    //$('.mq-textarea textarea').attr('disabled', 'true'); // Force keyboard to hide on textarea field.
-    setTimeout(function() {
-        // $('.mq-textarea textarea').blur();  //actually close the keyboard
-        // $('.mq-textarea textarea').removeAttr('readonly');
-        // $('.mq-textarea textarea').removeAttr('disabled');
-        openKeyboard();
-    }, 100);
+	setTimeout(function() {
+		openKeyboard();
+	}, 100);
   });
 
   $('#keyboard-mask').height($('#keyboard-wrapper').height());
+}
 
-});
+
+function closeKeyboard() {
+  $('#keyboard-mask').slideUp();
+  $('#keyboard-wrapper').slideUp();
+}
+
+function openKeyboard() {
+	$('#keyboard-wrapper').slideDown();
+	$('#keyboard-mask').slideDown("fast", function() {
+	  $(window).scrollTop($('#keyboard-mask').position().top + $('#keyboard-mask').outerHeight());
+	});
+}
+
+
+
+
