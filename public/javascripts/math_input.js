40ac4567afdd7d9a77d35fa261468fa66f264b53
HuyLafa <anhhuy0706@gmail.com>
2017-03-30 14:18:28 -0400

    Fix null pointer when math keyboard is removed.

--- public/javascripts/math_input.js
+++ public/javascripts/math_input.js
@@ -30,39 +30,45 @@ function setupMathInput() {
 
   $('#keyboard .keypad:not(.default)').hide();
   $('#keyboard .key').click( function(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	if ( $(this).data('action') == 'write') {
-	  mathField.write($(this).data('content'));
-	} else if($(this).data('action') == 'cmd') {
-	  mathField.cmd($(this).data('content'));
-	} else if($(this).data('action') == 'keystroke') {
-	  mathField.keystroke($(this).data('content'));
-	} else if($(this).data('action') == 'switch-keys') {
-	  console.log("'#keyboard .keypad:not(.' + $(this).data('content') + ')'");
-	  $('#keyboard .keypad:not(.' + $(this).data('content') + ')').hide();
-	  $('#keyboard .keypad.' + $(this).data('content')).show();
-	} else if($(this).data('action') == 'keyboard-hide'){
-	  closeKeyboard();
-	}
+    if ( mathField ) {
+      if ( $(this).data('action') == 'write') {
+            mathField.write($(this).data('content'));
+      }
+      else if($(this).data('action') == 'cmd') {
+        mathField.cmd($(this).data('content'));
+      }
+      else if($(this).data('action') == 'keystroke') {
+        mathField.keystroke($(this).data('content'));
+      }
+      else if($(this).data('action') == 'switch-keys') {
+        console.log("'#keyboard .keypad:not(.' + $(this).data('content') + ')'");
+        $('#keyboard .keypad:not(.' + $(this).data('content') + ')').hide();
+        $('#keyboard .keypad.' + $(this).data('content')).show();
+      }
+      else if($(this).data('action') == 'keyboard-hide'){
+        closeKeyboard();
+      }
 
-	if (typeof $(this).data('stepback') !== 'undefined') {
-	  for (var i = 0; i < parseInt($(this).data('stepback')); i++) {
-		mathField.keystroke('Left');
-	  }
-	}
+      if (typeof $(this).data('stepback') !== 'undefined') {
+        for (var i = 0; i < parseInt($(this).data('stepback')); i++) {
+        mathField.keystroke('Left');
+        }
+      }
 
-	if (typeof $(this).data('stepforward') !== 'undefined') {
-	  for (var i = 0; i < parseInt($(this).data('stepforward')); i++) {
-		mathField.keystroke('Right');
-	  }
-	}
+      if (typeof $(this).data('stepforward') !== 'undefined') {
+        for (var i = 0; i < parseInt($(this).data('stepforward')); i++) {
+        mathField.keystroke('Right');
+        }
+      }
 
-	if(isMobile) {
-	  mathField.blur();
-	} else {
-	  mathField.focus();
-	}
+      if(isMobile) {
+        mathField.blur();
+      } else {
+        mathField.focus();
+      }
+    }
   });
 
   closeKeyboard();
