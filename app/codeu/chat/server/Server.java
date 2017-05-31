4e76842061a862b4ec8d8e34f3b231f645ac5457
HuyLafa <anhhuy0706@gmail.com>
2017-05-30 22:49:52 -0400

    Move codeu/chat inside app/

--- app/codeu/chat/server/Server.java
+++ app/codeu/chat/server/Server.java
@@ -18,14 +18,11 @@ package codeu.chat.server;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Arrays;
 import java.util.Collection;
 
 import codeu.chat.common.Conversation;
 import codeu.chat.common.ConversationSummary;
-import codeu.chat.common.LinearUuidGenerator;
 import codeu.chat.common.Message;
 import codeu.chat.common.NetworkCode;
 import codeu.chat.common.Relay;
